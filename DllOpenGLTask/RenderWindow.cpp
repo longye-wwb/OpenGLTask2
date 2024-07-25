08539593fe0265da6dcf72eb647bd168d929fd61
longye-wwb <764743604@qq.com>
2024-07-25 19:21:52 +0800

    删除一些注释

--- DllOpenGLTask/RenderWindow.cpp
+++ DllOpenGLTask/RenderWindow.cpp
@@ -85,18 +85,6 @@ namespace openGLTask {
 
 		Shader LightingShader(m_VertShaderPath.c_str(), m_FragShaderPath.c_str());
 
-		//float Vertices[] = {
-		//	//Verices              Color             Normal
-		//	 0.5f, 0.5f, 0.0f,  1.0f,0.0f,0.0f,  0.0f,0.0f,1.0f,
-		//	 0.5f,-0.5f, 0.0f,  0.0f,1.0f,0.0f,  0.0f,0.0f,1.0f,
-		//	-0.5f,-0.5f, 0.0f,  0.0f,0.0f,1.0f,  0.0f,0.0f,1.0f,
-		//	-0.5f, 0.5f, 0.0f,  1.0f,1.0f,0.0f,  0.0f,0.0f,1.0f,
-		//};
-		//unsigned int Indices[] = {
-		//	0, 1, 3,
-		//	1, 2, 3
-		//};
-		
 		unsigned int VBO, VAO, EBO;
 		__setAndBindVertices();
 
@@ -133,11 +121,6 @@ namespace openGLTask {
 
 		glfwTerminate();
 
-		/*unsigned int VBO, VAO, EBO;
-		__setAndBindVertices(VBO, VAO, EBO);
-		__renderLoop();
-		__deleteBind(VBO, VAO, EBO);
-		glfwTerminate();*/
 		return ;
 	}
 
@@ -320,43 +303,7 @@ namespace openGLTask {
 
 	void CRenderWindow::__renderLoop()
 	{
-		HIVE_LOG_INFO("GLAD : {}", gladLoadGLLoader((GLADloadproc)glfwGetProcAddress));
-		Shader LightingShader("directionLight.vs", "directionLight.fs");
-		float Vertices[] = {
-			//Verices              Color             Normal
-			 0.5f, 0.5f, 0.0f,  1.0f,0.0f,0.0f,  0.0f,0.0f,1.0f,
-			 0.5f,-0.5f, 0.0f,  0.0f,1.0f,0.0f,  0.0f,0.0f,1.0f,
-			-0.5f,-0.5f, 0.0f,  0.0f,0.0f,1.0f,  0.0f,0.0f,1.0f,
-			-0.5f, 0.5f, 0.0f,  1.0f,1.0f,0.0f,  0.0f,0.0f,1.0f,
-		};
-		std::cout << std::size(Vertices);
-		unsigned int Indices[] = {
-			0, 1, 3,
-			1, 2, 3
-		};
-		unsigned int VBO, VAO, EBO;
-		glGenVertexArrays(1, &VAO);
-		glGenBuffers(1, &VBO);
-		glGenBuffers(1, &EBO);
-
-		glBindVertexArray(VAO);
-
-		glBindBuffer(GL_ARRAY_BUFFER, VBO);
-		glBufferData(GL_ARRAY_BUFFER, sizeof(Vertices), Vertices, GL_STATIC_DRAW);
-
-		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-		glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(Indices), Indices, GL_STATIC_DRAW);
-
-		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 9 * sizeof(float), (void*)0);
-		glEnableVertexAttribArray(0);
-		glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 9 * sizeof(float), (void*)(3 * sizeof(float)));
-		glEnableVertexAttribArray(1);
-		glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, 9 * sizeof(float), (void*)(6 * sizeof(float)));
-		glEnableVertexAttribArray(2);
-
-		glBindBuffer(GL_ARRAY_BUFFER, 0);
-
-		glm::vec3 CameraPos = glm::vec3(0.0f, 0.0f, 3.0f);
+	/*	glm::vec3 CameraPos = glm::vec3(0.0f, 0.0f, 3.0f);
 		glm::vec3 Up = glm::vec3(0.0f, 1.0f, 0.0f);
 		glm::vec3 Front = glm::vec3(0.0f, 0.0f, -1.0f);
 
@@ -378,17 +325,10 @@ namespace openGLTask {
 			glm::mat4 model = glm::mat4(1.0f);
 			LightingShader.setMat4("uModel", model);
 
-			glBindVertexArray(VAO);
-			glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-
 			glfwSwapBuffers(m_pWindow);
 			glfwPollEvents();
-		}
+		}*/
 
-
-		glDeleteVertexArrays(1, &VAO);
-		glDeleteBuffers(1, &VBO);
-		glDeleteBuffers(1, &EBO);
 	}
 
 	void CRenderWindow::__setAndBindVertices()
--- OpenGLTask1/SquareShading.cpp
+++ OpenGLTask1/SquareShading.cpp
@@ -12,23 +12,3 @@ int main()
     std::shared_ptr<openGLTask::CRenderWindow> pRenderWindow = std::make_shared<openGLTask::CRenderWindow>();
     pRenderWindow->startRun();
 }   
-//void customRender() {
-//    glm::vec3 CameraPos = glm::vec3(0.0f, 0.0f, 3.0f);
-//    glm::vec3 Up = glm::vec3(0.0f, 1.0f, 0.0f);
-//    glm::vec3 Front = glm::vec3(0.0f, 0.0f, -1.0f);
-//    glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
-//    glClear(GL_COLOR_BUFFER_BIT);
-//
-//    LightingShader.use();
-//    LightingShader.setVec3("uViewPos", CameraPos);
-//    LightingShader.setFloat("uShininess", 32.0f);
-//    LightingShader.setFloat("uAmbientStrength", 0.1f);
-//
-//    glm::mat4 ProjectionMat = glm::perspective(glm::radians(45.0f), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
-//    glm::mat4 ViewMat = glm::lookAt(CameraPos, CameraPos + Front, Up);
-//    LightingShader.setMat4("uProjection", ProjectionMat);
-//    LightingShader.setMat4("uView", ViewMat);
-//
-//    glm::mat4 model = glm::mat4(1.0f);
-//    LightingShader.setMat4("uModel", model);
-//}
