da25baf422691adafe29c4318797db86f8ea3f85
longye-wwb <764743604@qq.com>
2024-07-26 19:33:39 +0800

    开始

--- DllOpenGLTask/simpleMaterial.h
+++ DllOpenGLTask/simpleMaterial.h
@@ -1,12 +1,12 @@
 #pragma once
 #include "Material.h"
-#include ""
+
 namespace openGLTask 
 {
 	class simpleMaterial :public CMaterial
 	{
 	private:
-		std::shared_ptr<CShader> m_pShader;
+		//std::shared_ptr<CShader> m_pShader;
 	};
 
 }
